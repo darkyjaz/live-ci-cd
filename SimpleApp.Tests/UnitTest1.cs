34aa20e17d74aaa5efd3ab05472c903cfa437906
darkyjaz <jazzyxu95@gmail.com>
2022-02-10 01:06:38 +1100

    update

---
+++ SimpleApp.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace SimpleApp.Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+            Assert.True(1 == 1);
+
+        }
+
+        [Fact]
+        public void Test2() {
+            Assert.True(1 == 0);
+
+        }
+    }
+}
